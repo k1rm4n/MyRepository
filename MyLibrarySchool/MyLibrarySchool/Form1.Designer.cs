8fdd6acd9c730b504bcb91a8c98cb1a1d03afea4
k1rm4n <81237385+k1rm4n@users.noreply.github.com>
2021-12-08 17:05:46 +0300

    Add files via upload

---
+++ MyLibrarySchool/MyLibrarySchool/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace MyLibrarySchool
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.toolStrip2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.toolStrip1);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 422);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Students";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(3, 3);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(786, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(47, 22);
+            this.toolStripButton1.Text = "Restart";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(786, 388);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            this.dataGridView1.UserAddedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridView1_UserAddedRow);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Controls.Add(this.toolStrip2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 422);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Library";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 31);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 25;
+            this.dataGridView2.Size = new System.Drawing.Size(786, 388);
+            this.dataGridView2.TabIndex = 1;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            this.dataGridView2.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellValueChanged);
+            this.dataGridView2.UserAddedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridView2_UserAddedRow);
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton2});
+            this.toolStrip2.Location = new System.Drawing.Point(3, 3);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(786, 25);
+            this.toolStrip2.TabIndex = 0;
+            this.toolStrip2.Text = "toolStrip2";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(47, 22);
+            this.toolStripButton2.Text = "Restart";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.button1);
+            this.tabPage3.Controls.Add(this.textBox1);
+            this.tabPage3.Controls.Add(this.dataGridView3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 24);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(792, 422);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Search";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(3, 31);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowTemplate.Height = 25;
+            this.dataGridView3.Size = new System.Drawing.Size(786, 388);
+            this.dataGridView3.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(8, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView3;
+    }
+}
+
---
+++ MyLibrarySchool/MyLibrarySchool/Form1.cs
@@ -0,0 +1,423 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace MyLibrarySchool
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection sqlConnection = null;
+
+        private SqlCommandBuilder sqlBuilderLibrary = null;
+
+        private SqlDataAdapter sqlDataAdapterLibrary = null;
+
+        private DataSet dataSetLibrary = null;
+
+
+        private SqlCommandBuilder sqlBuilderStudents = null;
+
+        private SqlDataAdapter sqlDataAdapterStudents = null;
+
+        private DataSet dataSetStudents = null;
+
+        private bool newRowAdding = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadData_Students()
+        {
+            try
+            {
+                sqlDataAdapterStudents = new SqlDataAdapter("SELECT *, 'Delete' AS [Delete] FROM Students", sqlConnection);
+
+                sqlBuilderStudents = new SqlCommandBuilder(sqlDataAdapterStudents);
+
+                sqlBuilderStudents.GetInsertCommand();
+                sqlBuilderStudents.GetUpdateCommand();
+                sqlBuilderStudents.GetDeleteCommand();
+
+                dataSetStudents = new DataSet();
+
+                sqlDataAdapterStudents.Fill(dataSetStudents, "Students");
+
+                dataGridView1.DataSource = dataSetStudents.Tables["Students"];
+
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+
+                    dataGridView1[5, i] = linkCell;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void LoadData_Library()
+        {
+            try
+            {
+                sqlDataAdapterLibrary = new SqlDataAdapter("SELECT *, 'Delete' AS [Delete] FROM Library", sqlConnection);
+
+                sqlBuilderLibrary = new SqlCommandBuilder(sqlDataAdapterLibrary);
+
+                sqlBuilderLibrary.GetInsertCommand();
+                sqlBuilderLibrary.GetUpdateCommand();
+                sqlBuilderLibrary.GetDeleteCommand();
+
+                dataSetLibrary = new DataSet();
+
+                sqlDataAdapterLibrary.Fill(dataSetLibrary, "Library");
+
+                dataGridView2.DataSource = dataSetLibrary.Tables["Library"];
+
+                for (int i = 0; i < dataGridView2.Rows.Count; i++)
+                {
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+
+                    dataGridView2[3, i] = linkCell;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ReloadData_Students()
+        {
+            try
+            {
+                dataSetStudents.Tables["Students"].Clear();
+
+                sqlDataAdapterStudents.Fill(dataSetStudents, "Students");
+
+                dataGridView1.DataSource = dataSetStudents.Tables["Students"];
+
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+
+                    dataGridView1[5, i] = linkCell;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void ReloadData_Library()
+        {
+            try
+            {
+                dataSetLibrary.Tables["Library"].Clear();
+
+                sqlDataAdapterLibrary.Fill(dataSetLibrary, "Library");
+
+                dataGridView2.DataSource = dataSetLibrary.Tables["Library"];
+
+                for (int i = 0; i < dataGridView2.Rows.Count; i++)
+                {
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+
+                    dataGridView2[3, i] = linkCell;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["DataBase"].ConnectionString);
+
+            sqlConnection.Open();
+
+            LoadData_Students();
+            LoadData_Library();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (e.ColumnIndex == 5)
+                {
+                    string task = dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();
+
+                    if (task == "Delete")
+                    {
+                        if (MessageBox.Show("Удалить эту строку?", "Удаление", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            int rowIndex = e.RowIndex;
+
+                            dataGridView1.Rows.RemoveAt(rowIndex);
+
+                            dataSetStudents.Tables["Students"].Rows[rowIndex].Delete();
+
+                            sqlDataAdapterStudents.Update(dataSetStudents, "Students");
+                        }
+                    }
+                    else if (task == "Insert")
+                    {
+                        int rowIndex = dataGridView1.Rows.Count - 2;
+
+                        DataRow row = dataSetStudents.Tables["Students"].NewRow();
+                        row["name"] = dataGridView1.Rows[rowIndex].Cells["name"].Value;
+                        row["surname"] = dataGridView1.Rows[rowIndex].Cells["surname"].Value;
+                        row["class"] = dataGridView1.Rows[rowIndex].Cells["class"].Value;
+                        row["id_book"] = dataGridView1.Rows[rowIndex].Cells["id_book"].Value;
+                       
+
+                        dataSetStudents.Tables["Students"].Rows.Add(row);
+
+                        dataSetStudents.Tables["Students"].Rows.RemoveAt(dataSetStudents.Tables["Students"].Rows.Count - 1);
+
+                        dataGridView1.Rows.RemoveAt(dataGridView1.Rows.Count - 2);
+
+                        dataGridView1.Rows[e.RowIndex].Cells[5].Value = "Delete";
+
+                        sqlDataAdapterStudents.Update(dataSetStudents, "Students");
+
+                        newRowAdding = false;
+                    }
+                    else if (task == "Update")
+                    {
+                        int r = e.RowIndex;
+                        dataSetStudents.Tables["Students"].Rows[r]["name"] = dataGridView1.Rows[r].Cells["name"].Value;
+                        dataSetStudents.Tables["Students"].Rows[r]["surname"] = dataGridView1.Rows[r].Cells["surname"].Value;
+                        dataSetStudents.Tables["Students"].Rows[r]["class"] = dataGridView1.Rows[r].Cells["class"].Value;
+                        dataSetStudents.Tables["Students"].Rows[r]["id_book"] = dataGridView1.Rows[r].Cells["id_book"].Value;
+
+                        sqlDataAdapterStudents.Update(dataSetStudents, "Students");
+
+                        dataGridView1.Rows[e.RowIndex].Cells[5].Value = "Delete";
+
+                    }
+                }
+                ReloadData_Students();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView1_UserAddedRow(object sender, DataGridViewRowEventArgs e)
+        {
+            try
+            {
+                if (newRowAdding == false)
+                {
+                    newRowAdding = true;
+
+                    int lastRow = dataGridView1.Rows.Count - 2;
+
+                    DataGridViewRow row = dataGridView1.Rows[lastRow];
+
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+
+                    dataGridView1[5, lastRow] = linkCell;
+
+                    row.Cells["Delete"].Value = "Insert";
+                }
+            }
+
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (newRowAdding == false)
+                {
+                    int rowIndex = dataGridView1.SelectedCells[0].RowIndex;
+
+                    DataGridViewRow editingRow = dataGridView1.Rows[rowIndex];
+
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+
+                    dataGridView1[5, rowIndex] = linkCell;
+
+                    editingRow.Cells["Delete"].Value = "Update";
+                }
+            }
+
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (e.ColumnIndex == 3)
+                {
+                    string task = dataGridView2.Rows[e.RowIndex].Cells[3].Value.ToString();
+
+                    if (task == "Delete")
+                    {
+                        if (MessageBox.Show("Удалить эту строку?", "Удаление", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            int rowIndex = e.RowIndex;
+
+                            dataGridView2.Rows.RemoveAt(rowIndex);
+
+                            dataSetLibrary.Tables["Library"].Rows[rowIndex].Delete();
+
+                            sqlDataAdapterLibrary.Update(dataSetLibrary, "Library");
+                        }
+                    }
+                    else if (task == "Insert")
+                    {
+                        int rowIndex = dataGridView2.Rows.Count - 2;
+
+                        DataRow row = dataSetLibrary.Tables["Library"].NewRow();
+
+                        row["Id"] = dataGridView2.Rows[rowIndex].Cells["Id"].Value;
+                        row["name_book"] = dataGridView2.Rows[rowIndex].Cells["name_book"].Value;
+                        row["author"] = dataGridView2.Rows[rowIndex].Cells["author"].Value;
+
+
+                        dataSetLibrary.Tables["Library"].Rows.Add(row);
+
+                        dataSetLibrary.Tables["Library"].Rows.RemoveAt(dataSetLibrary.Tables["Library"].Rows.Count - 1);
+
+                        dataGridView2.Rows.RemoveAt(dataGridView2.Rows.Count - 2);
+
+                        dataGridView2.Rows[e.RowIndex].Cells[3].Value = "Delete";
+
+                        sqlDataAdapterLibrary.Update(dataSetLibrary, "Library");
+
+                        newRowAdding = false;
+                    }
+                    else if (task == "Update")
+                    {
+                        int r = e.RowIndex;
+
+                        dataSetLibrary.Tables["Library"].Rows[r]["Id"] = dataGridView2.Rows[r].Cells["Id"].Value;
+                        dataSetLibrary.Tables["Library"].Rows[r]["name_book"] = dataGridView2.Rows[r].Cells["name_book"].Value;
+                        dataSetLibrary.Tables["Library"].Rows[r]["author"] = dataGridView2.Rows[r].Cells["author"].Value;
+
+                        sqlDataAdapterLibrary.Update(dataSetStudents, "Library");
+
+                        dataGridView2.Rows[e.RowIndex].Cells[3].Value = "Delete";
+
+                    }
+
+                    ReloadData_Library();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView2_UserAddedRow(object sender, DataGridViewRowEventArgs e)
+        {
+            try
+            {
+                if (newRowAdding == false)
+                {
+                    newRowAdding = true;
+
+                    int lastRow = dataGridView2.Rows.Count - 2;
+
+                    DataGridViewRow row = dataGridView2.Rows[lastRow];
+
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+
+                    dataGridView2[3, lastRow] = linkCell;
+
+                    row.Cells["Delete"].Value = "Insert";
+                }
+            }
+
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView2_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (newRowAdding == false)
+                {
+                    int rowIndex = dataGridView2.SelectedCells[0].RowIndex;
+
+                    DataGridViewRow editingRow = dataGridView2.Rows[rowIndex];
+
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+
+                    dataGridView2[3, rowIndex] = linkCell;
+
+                    editingRow.Cells["Delete"].Value = "Update";
+                }
+            }
+
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            ReloadData_Students();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            ReloadData_Library();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(
+                            $"SELECT Students.name, Students.surname, Students.class, Library.name_book, Library.author FROM Students JOIN Library ON Students.id_book = Library.Id WHERE Students.id_book = {textBox1.Text}", sqlConnection);
+
+            DataSet dataSet = new DataSet();
+
+            sqlDataAdapter.Fill(dataSet);
+
+            dataGridView3.DataSource = dataSet.Tables[0].DefaultView;
+        }
+    }
+}
